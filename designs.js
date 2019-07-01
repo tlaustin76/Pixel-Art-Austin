7696eb48a8fd4feb2433f3f33efaa0a8e9861311
tlaustin76 <52425161+tlaustin76@users.noreply.github.com>
2019-07-01 17:24:45 -0400

    Update designs.js

--- designs.js
+++ designs.js
@@ -26,7 +26,7 @@ function makeGrid(x, y) {
   $('td').click(function addColor() {
       color = $('#colorPicker').val();
 
-      if ($(this).attr(style')) {
+      if ($(this).attr('style')) {
           $(this).removeAttr('style')
       } else {
           $(this).attr('style', 'background-color:' + color);
