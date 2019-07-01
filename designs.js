ccb1703c3c37afceff23c969cc25b50909279293
tlaustin76 <52425161+tlaustin76@users.noreply.github.com>
2019-07-01 15:15:36 -0400

    Update designs.js

--- designs.js
+++ designs.js
@@ -1,10 +1,36 @@
 // Select color input
 // Select size input
+var height, width, color;
 
 // When size is submitted by the user, call makeGrid()
+$('#sizepicker').submit(function (event) {
+    event.preventDefault();
+    height = $('#inputHeight').val();
+    width = $('#inputWidth').val();
+    makeGrid(height, width);
+})
 
-function makeGrid() {
+function makeGrid(x, y) {
+  $('tr').remove();
 
 // Your code goes here!
+  for (var i = 1; 1 <= x; i++) {
+      $('#pixelCanvas').append('<tr id=table' + 1 + '></tr>');
+      for (var j = 1; j <=; j++) {
+          $('#table' + i).append('<td></td>');
+        
+       }
+  }
 
+  // adding a color
+
+  $('td').click(function addColor) {
+      color = $('#colorPicker').val();
+
+      if ($(this).attr(style')) {
+          $(this).removeAttr('style')
+      } else {
+          $(this).attr('style', 'background-color:' + color);
+      }
+  })
 }
